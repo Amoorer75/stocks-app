bab2ea53bde64bd0866c7feeac4327aca1f18d25
Amoorer75 <Amoorer127@gmail.com>
2022-02-20 20:44:17 -0500

    first init

--- src/componets/SearchStock.js
+++ src/componets/SearchStock.js
@@ -6,13 +6,15 @@ export default function SearchStock(){
     const [stock,setStock] = useState('')
 
    console.log(stock)
-
+const handelInput = (text) =>{
+    setStock(text)
+}
 
    {<ShowStockInfo input={stock} />}
 
     return(
         <div>
-            <input onChange={(text) => setStock(text.target.value)}></input>
+            <input onChange={(text) => handelInput(text.target.value)}></input>
         </div>
     )
 }
